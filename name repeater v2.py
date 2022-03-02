de48b8f09d37ffe490f6a7828e2330be95cb3796
T360-v2-DTPB-s07 <pallagipeter@gmail.com>
2022-03-02 16:54:41 +0100

    test

--- name repeater v2.py
+++ name repeater v2.py
@@ -6,3 +6,10 @@ name = input("whats your name")
 multiplier = int(input ("how many times you want to see it"))
 for i in range (multiplier):
     print(name)
+
+
+
+#irasd ki hogy az év egyik honapja januar az év egyik honapja febr stb
+count=0
+for mounth in ("januar", "februar" március)
+    print("az év +str(count) +" honapja" + month + ".")
