f8ca6fb5e57726098bc8f94736afcc57559d3e1c
T360-v2-DTPB-s07 <pallagipeter@gmail.com>
2022-03-02 16:15:00 +0100

    test

---
+++ venv/name repeater.py
@@ -0,0 +1,13 @@
+
+#kérd be a nevet
+#hérdezd meg hányszor
+#ird ki annyiszor
+
+name = input("whats your name")
+multiplier = int(input ("how many times you want to see it"))
+for i in range (multiplier):
+    print(name)
+
+
+
+
