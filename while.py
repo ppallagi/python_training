2e786f6a425aad4817b3252d52148b4c2fd40425
T360-v2-DTPB-s07 <pallagipeter@gmail.com>
2022-03-02 16:47:05 +0100

    test

---
+++ while.py
@@ -0,0 +1,11 @@
+#while ciklus
+#addig kérj be a felhazsnálótól egy számot amig 0 át nem ir be
+# ird ki a bekért szám kétszeresét
+
+number = 10
+while number !=0:
+    number = int(input("give me a number"))
+    print(number*2)
+
+#az elején a number = 10 azért van hoyg egyáltalán elinduljon:)
+
