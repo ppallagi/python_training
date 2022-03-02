d59905da44758cb391caf61f8a3dc37ab94dc69d
T360-v2-DTPB-s07 <pallagipeter@gmail.com>
2022-03-02 15:47:24 +0100

    test

--- variables.py
+++ variables.py
@@ -16,10 +16,17 @@ print(type(age))
 age = 36
 print(type(age))
 
+# hozzatok létre egy változót, hogy órák száma, hours, értéke legyen először 3
+# hozzatok létre egy másik változót, neve: minutes, tartalmazza, hogy - előző változó szorzása 60-nal
+# 3 óra az 180 perc - számok helyén használjátok a hours, és minutes változókat
 
 hours =3
 minutes = hours*60
 print(str(hours) + "óra az" + str(minutes) + "perc")
+
+# hozzatok létre egy word nevű változót, és adjátok neki értékül egy nagyon hosszú szót
+# számoljátok ki ennek a hosszát
+# A xxxcxghedtwireothwe pontosan 13 karakter
 
 
 word = "megkáposztatatlanizéke"
